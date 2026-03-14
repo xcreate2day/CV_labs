c303e2232e791995849bf4f76e76ec61b52bd32c
xcreate2day <104548518+xcreate2day@users.noreply.github.com>
2026-03-14 12:05:48 +0300

    Create tst.py

---
+++ lab1/tst.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+a = np.arange(12).reshape(3, 4)
+print(a)
+
+i = np.array(
+    [
+        [0, 1],  # indices for the first dim of `a`
+        [1, 2],
+    ]
+)
+
+j = np.array(
+    [
+        [2, 1],  # indices for the second dim
+        [3, 3],
+    ]
+)
+print()
+print(a[i, j])
